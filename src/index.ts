85d46aac389023dc2d71123a70437eb4cc68fb1b
Michael Arnaldi <arnaldimichael@gmail.com>
2022-11-24 14:43:34 +0000

    restrict zoom to only objects with string keys

--- src/index.ts
+++ src/index.ts
@@ -349,38 +349,51 @@ export type ZoomerTypeId = typeof ZoomerTypeId
 /**
  * @since 1.0.0
  */
-export type Zoomer<S> = {
+export type FocusStructure<S> = {
   [K in keyof S | ZoomerTypeId]: K extends ZoomerTypeId ? ZoomerTypeId
-    : K extends keyof S ? Zoomer<S[K]>
+    : K extends keyof S ? keyof S[K] extends string ? FocusStructure<S[K]>
+    : FocusPrimitive<S[K]>
     : never
 }
+
+/**
+ * @since 1.0.0
+ */
+export type FocusPrimitive<S> = {
+  [ZoomerTypeId]: (_: S) => S
+}
+
+/**
+ * @since 1.0.0
+ */
+export type FocusInitial<S> = keyof S extends string ? FocusStructure<S> : FocusPrimitive<S>
+
+const focus = <S>(
+  ops: Array<PropertyKey> = []
+): FocusStructure<S> =>
+  // @ts-expect-error
+  new Proxy(new Function(), {
+    get: (_, prop) => {
+      if (prop === ZoomerTypeId) {
+        return ops
+      }
+      return focus([...ops, prop])
+    }
+  })
 
 /**
  * An optic that accesses a nested field of a struct.
  *
  * @since 1.0.0
  */
-export const zoom = <S, A>(f: (s: Zoomer<S>) => Zoomer<A>): Lens<S, A> => {
-  const path: Array<string | symbol> = []
-  const detector = (): any =>
-    new Proxy({}, {
-      get: (_target, prop, rec) => {
-        if (rec !== lastDetector) {
-          throw new Error("Invalid `zoom` pattern detected")
-        }
-        path.push(prop)
-        lastDetector = detector()
-        return lastDetector
-      }
-    })
-  let lastDetector = detector()
-  const a = f(lastDetector)
-  if (a !== lastDetector) {
-    throw new Error("Invalid `zoom` pattern detected")
-  }
-  let out: Lens<S, any> = id<S>()
-  for (const k of path) {
-    out = out.compose(key(k))
+export const zoom: {
+  <S, A>(f: (s: FocusInitial<S>) => FocusStructure<A>): Lens<S, A>
+  <S, A>(f: (s: FocusInitial<S>) => FocusPrimitive<A>): Lens<S, A>
+} = (f: any): any => {
+  const x = f(focus() as any)
+  let out: Lens<any, any> | Optional<any, any> = id()
+  for (const k of (x[ZoomerTypeId] as unknown as Array<PropertyKey>)) {
+    out = out.compose(key(k as any))
   }
   return out
 }
