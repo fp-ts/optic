8ad95f3dbe8e19563bcb91d9da6316d43718fe97
gcanti <giulio.canti@gmail.com>
2022-11-26 05:16:16 +0100

    add /data/string

--- test/examples.ts
+++ test/examples.ts
@@ -1,10 +1,13 @@
+import * as O from "@fp-ts/data/Option"
 import * as Optic from "@fp-ts/optic"
+import * as OptionOptic from "@fp-ts/optic/data/Option"
+import * as StringOptic from "@fp-ts/optic/data/string"
 
 describe("examples", () => {
   it("README", () => {
     interface Street {
       num: number
-      name: string
+      name: O.Option<string>
     }
     interface Address {
       city: string
@@ -13,14 +16,13 @@ describe("examples", () => {
     interface Company {
       name: string
       address: Address
-      owners: ReadonlyArray<string>
     }
     interface Employee {
       name: string
       company: Company
     }
 
-    const employee: Employee = {
+    const from: Employee = {
       name: "john",
       company: {
         name: "awesome inc",
@@ -28,37 +30,38 @@ describe("examples", () => {
           city: "london",
           street: {
             num: 23,
-            name: "high street"
-          }
-        },
-        owners: ["mike"]
-      }
-    }
-
-    const capitalize = (s: string): string => s.substring(0, 1).toUpperCase() + s.substring(1)
-
-    const employeeCapitalized = {
-      ...employee,
-      company: {
-        ...employee.company,
-        address: {
-          ...employee.company.address,
-          street: {
-            ...employee.company.address.street,
-            name: capitalize(employee.company.address.street.name)
+            name: O.some("high street")
           }
         }
       }
     }
 
-    const _name = Optic.id<Employee>()
-      .compose(Optic.key("company"))
-      .compose(Optic.key("address"))
-      .compose(Optic.key("street"))
-      .compose(Optic.key("name"))
+    const to: Employee = {
+      name: "john",
+      company: {
+        name: "awesome inc",
+        address: {
+          city: "london",
+          street: {
+            num: 23,
+            name: O.some("High street")
+          }
+        }
+      }
+    }
+
+    const _name: Optic.Optional<Employee, string> = Optic.id<Employee>()
+      .compose(Optic.key("company")) // Lens<Employee, Company>
+      .compose(Optic.key("address")) // Lens<Employee, Company>
+      .compose(Optic.key("street")) // Lens<<Employee, Company>
+      .compose(Optic.key("name")) // Lens<Street, O.Option<string>>
+      .compose(OptionOptic.some()) // Prism<O.Option<string>, string>
+      .compose(StringOptic.index(0)) // Optional<string, string>
+
+    const capitalize = (s: string): string => s.toUpperCase()
 
     const capitalizeName = Optic.modify(_name)(capitalize)
 
-    expect(capitalizeName(employee)).toEqual(employeeCapitalized)
+    expect(capitalizeName(from)).toEqual(to)
   })
 })
