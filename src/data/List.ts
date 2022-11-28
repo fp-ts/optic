8426d13337bed559dced112c3a4be47258f732f6
gcanti <giulio.canti@gmail.com>
2022-11-28 19:05:28 +0100

    Chunk, List: add head, tail

--- src/data/List.ts
+++ src/data/List.ts
@@ -4,12 +4,13 @@
 import * as E from "@fp-ts/data/Either"
 import type { List } from "@fp-ts/data/List"
 import * as L from "@fp-ts/data/List"
-import type { PolyPrism, Prism } from "@fp-ts/optic"
+import type { Optional, PolyPrism, Prism } from "@fp-ts/optic"
 import * as Optic from "@fp-ts/optic"
 
 /**
  * An optic that accesses the `Cons` case of a `List`.
  *
+ * @category constructors
  * @since 1.0.0
  */
 export const cons: {
@@ -23,3 +24,15 @@ export const cons: {
         E.left(Error("isCons")),
     ([head, tail]) => L.cons(head, tail)
   )
+
+/**
+ * @category constructors
+ * @since 1.0.0
+ */
+export const head = <A>(): Optional<List<A>, A> => cons<A>().at("0")
+
+/**
+ * @category constructors
+ * @since 1.0.0
+ */
+export const tail = <A>(): Optional<List<A>, List<A>> => cons<A>().at("1")
